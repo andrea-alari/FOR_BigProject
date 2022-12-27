17ad0a785b577ab57b284ae61995446286aa525f
andrea-alari <andrea.alari@mail.polimi.it>
2022-12-27 11:19:25 +0100

    refactor of results file. Now the format is csv

--- main.py
+++ main.py
@@ -22,23 +22,21 @@ def solve(instance):
 
 
 def write_results():
-    out = open("results.txt", "w")
-
+    out = open("results.csv", "w")
+    out.write("ID, OBJ, TIME\n")
     for i in range(1, 11):
         inst = load_instance("instances/instance_{}.json".format(i))
         st = time.time()
         res = solve(inst)
         et = time.time()
-        out.write("instance {instance:} :value-> {value:2f}      exec_time ->{time:2f}\n".format(instance=i, value=res,
-                                                                                                 time=(et - st)))
+        out.write("{instance:},{value:2f},{time:2f}\n".format(instance=i, value=res, time=(et - st)))
     out.close()
-
 
 
 write_results()
 '''
-inst = load_instance("instances/instance_1.json")
-res = load_result("solutions/instance_1.txt")
+inst = load_instance("instances/instance_4.json")
+res = load_result("solutions/instance_4.txt")
 
 obj = solve(inst)
 
